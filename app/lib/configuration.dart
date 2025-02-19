9c72fe0f91285007bc841d703a98a04cdf86e2a5
Melvin Massotti <massotti.melvin@gmail.com>
2025-02-19 12:49:09 +0100

    fixed internet connection http

--- app/lib/configuration.dart
+++ app/lib/configuration.dart
@@ -1,6 +1,6 @@
 class Configuration {
-  static const String BASE_URL = 'https://loved-finally-trout.ngrok-free.app';
+  static const String BASE_URL = 'http://35.156.137.121:8000';
   static const Duration PROCESSING_TIME_INTERVAL = Duration(seconds: 20);
   static const Duration PREDICTION_REQUEST_TIME_INTERVAL = Duration(milliseconds: 500);
-  static const int PREDICTION_SIZE = 3;
+  static const int PREDICTION_SIZE = 1;
 }
